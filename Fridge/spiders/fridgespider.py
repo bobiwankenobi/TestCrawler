602e3661df50c08adfc03f26b7643cbed97cde6f
bobiwankenobi <bobiwankenobii@gmail.com>
2017-03-20 14:26:51 +0200

    change for multiple pages

--- Fridge/spiders/fridgespider.py
+++ Fridge/spiders/fridgespider.py
@@ -6,11 +6,18 @@ from scrapy.http import Request
 class FridgeSpider(Spider):
 	name = "Fridgy"
 	allowed_domains = ["http://www.technomarket.bg"]
-	start_urls = ["http://www.technomarket.bg/hladilnitzi"]
+
+	start_urls_generator = (("http://www.technomarket.bg/product/filter?filter_form[sort]=price&filter_form[price][min]=159"
+							"&filter_form[price][max]=4599&filter_form[spec_volume_refrigerator][min]="
+							"&filter_form[spec_volume_refrigerator][max]=&filter_form[spec_volume_freezer][min]="
+							"&filter_form[spec_volume_freezer][max]="
+							"&filter_key=%2Fhladilnitzi%7Cstatic%7Cstatic&from={}&size=20").format(items*20) for items in range(0,20))
+
+	start_urls = list(start_urls_generator)
 
 	def parse(self, response):
 		for path in response.xpath('//figcaption'):
 			item = FridgeCrawlItem()
-			item["price"] = path.xpath('div[2]/var/span[1]/text()').extract()
-			item["title"] = path.xpath('div[1]/h3/a/span/text()').extract()
+			item["price"] = path.xpath('div/var/span/text()').extract()
+			item["title"] = path.xpath('div/h3/a/span/text()').extract()
 			yield item
